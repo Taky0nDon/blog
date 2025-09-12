d59d33d81d5be8fd7985fabda3f936906652e6ab
Taky0ndon <mmaggio89@gmail.com>
2025-09-12 18:08:16 -0400

    find relevant dom elements and create an array of the displayed blog post links

---
+++ static/html/script.js
@@ -0,0 +1,7 @@
+const postDiv = document.getElementById("postDisplay")
+const titleDiv = document.getElementById("postTitle")
+const dateDiv = document.getElementById("postDate")
+const articleText = document.getElementById("postContent")
+ 
+const buttons = document.querySelectorAll(".blogPostButton")
+console.log(buttons)
