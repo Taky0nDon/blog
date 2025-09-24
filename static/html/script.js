5d9d33f09b98104e136a82ebd9894d1f30aee40b
Taky0ndon <mmaggio89@gmail.com>
2025-09-24 19:46:07 -0400

    I defined the callback for the event instead of passing an anonymous function to it at call time. I believe this will make it easier for me to further act on the JSON sent in the response (to update the DOM with it).

--- static/html/script.js
+++ static/html/script.js
@@ -1,3 +1,17 @@
+const getTitle = (b) => {
+    console.log(typeof(b));
+    const urlToQueryPhp = "http://localhost:8181/php/ReadDb.php";
+    const postTitle = b.target.innerHTML;
+    const postData = fetch(urlToQueryPhp, {
+        method: "POST",
+        headers: { "Content-Type": "application/json", },
+        body: JSON.stringify({ id: postTitle })
+    }).
+        then(response => response.json()).
+        then(data => console.log(data));
+    console.log(postData);
+}
+
 const postDiv = document.getElementById("postDisplay");
 const titleDiv = document.getElementById("postTitle");
 const dateDiv = document.getElementById("postDate");
@@ -6,17 +20,5 @@ const articleText = document.getElementById("postContent");
 const buttons = document.querySelectorAll(".blogPostButton");
 
 for (const button of buttons){
-    button.addEventListener("click", () => {
-        const urlToQueryPhp = "http://localhost:3000/ReadDb.php";
-        const postTitle = button.innerHTML;
-        const postData = fetch(urlToQueryPhp, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ id: postTitle }),
-            }).
-                then(response => response.json()).
-                then(data => console.log(data));
-    });
+    button.addEventListener("click", getTitle);
 }
