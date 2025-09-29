fab04ce76ea2252f858870b52524b7d11a502fd6
Taky0ndon <mmaggio89@gmail.com>
2025-09-29 09:24:53 -0400

    modified CORS header to reflect prod domain

--- php/ReadDb.php
+++ php/ReadDb.php
@@ -4,7 +4,7 @@ function _log($message) {
 }
 
 header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Origin: writethyself.net");
 header("Access-Control-Allow-Methods: GET,POST,OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
